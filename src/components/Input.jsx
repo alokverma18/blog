ff8a4aed2f2d05203a62cb6c6274896ecb661864
programmer-rahul <7678478061k@gmail.com>
2024-07-17 20:48:47 +0530

    Fixed preview image in new-post & update-post

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,29 +1,33 @@
-import React, {useId} from 'react'
+import React, { useId } from "react";
 
-const Input = React.forwardRef( function Input({
+const Input = React.forwardRef(function Input(
+  {
     label,
     type = "text",
     className = "",
+    onChangeHandler = () => {},
     ...props
-}, ref){
-    const id = useId()
-    return (
-        <div className='w-full'>
-            {label && <label 
-            className='inline-block mb-1 pl-1' 
-            htmlFor={id}>
-                {label}
-            </label>
-            }
-            <input
-            type={type}
-            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-            ref={ref}
-            {...props}
-            id={id}
-            />
-        </div>
-    )
-})
+  },
+  ref
+) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="inline-block mb-1 pl-1" htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <input
+        type={type}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        ref={ref}
+        {...props}
+        id={id}
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+});
 
-export default Input
+export default Input;
